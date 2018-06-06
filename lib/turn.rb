8654cc1f8b2421cbe6181f0f1a2f018120a663bb
Ian Hollander <ianholla@gmail.com>
2018-06-06 11:07:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
     true
   else
     false
